a1e7129543721f5dc5b48cc44b683854d00d830e
varajala <valtteri.rajalainen@gmail.com>
2022-06-26 20:23:39 +0300

    Added usage info into README

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # react-project-template
+
 Script for creating a React project with Typescript and Webpack.
+
+## Use
+
+Make the script executable with
+
+    chmod u+x react-project-template.sh
+
+Run the script
+
+    ./react-project-template.sh "your-project-name-here"
+
+This will create a new directory with the project name and create the following directory structure:
+```
+.
+├── .gitignore
+├── package.json
+├── public
+│   └── index.html
+├── src
+│   └── index.tsx
+├── tsconfig.json
+└── webpack.config.js
+
+```
+
+Navigate into the project directory and install all required packages with `npm install`.
+Then build the app with `webpack` and start the dev server with `npm start`.
+
+Change the package information in `package.json` if needed.
+
---
+++ react-project-template.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+project_name="react-app"
+test ! -z "$1" && project_name="$1"
+
+mkdir $project_name
+cd $project_name
+git init
+
+cat << EOF > .gitignore
+node_modules
+public/build
+package-lock.json
+EOF
+
+cat << EOF > package.json
+{
+  "name": "$project_name",
+  "version": "1.0.0",
+  "description": "React project template with Typescript and Webpack",
+  "main": "src/index.tsx",
+  "scripts": {
+    "build": "webpack -p",
+    "start": "webpack-dev-server --devtool eval --config ./webpack.config.js"
+  },
+  "author": "$(git config user.name)",
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0"
+  },
+  "devDependencies": {
+    "@types/react": "^18.0.14",
+    "@types/react-dom": "^18.0.5",
+    "ts-loader": "^9.3.1",
+    "typescript": "^4.7.4",
+    "webpack": "^5.73.0",
+    "webpack-cli": "^4.10.0",
+    "webpack-dev-server": "^4.9.2"
+  }
+}
+EOF
+
+cat << EOF > tsconfig.json
+{
+  "compilerOptions": {
+    "sourceMap": true,
+    "module": "commonjs",
+    "target": "es5",
+    "jsx": "react"
+  },
+  "include": [
+    "src"
+  ],
+  "compileOnSave": false
+}
+EOF
+
+cat << EOF > webpack.config.js
+module.exports = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  output: {
+    path: __dirname + '/public/build',
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  }
+}
+EOF
+
+mkdir public
+cat << EOF > public/index.html
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>$project_name</title>
+    </head>
+
+    <body>
+        <div id="root"></div>
+        <script src="./build/app.js"></script>
+    </body>
+</html>
+EOF
+
+mkdir src
+cat << EOF > src/index.tsx
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>$project_name</h1>);
+EOF
+
